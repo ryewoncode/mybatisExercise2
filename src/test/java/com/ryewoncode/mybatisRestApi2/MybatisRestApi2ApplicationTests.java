2264ae87f19548079bdb0970753b0f718c449247
Ryewon <mykky0919@gmail.com>
2020-12-02 23:12:35 +0900

    INIT: generate base file

---
+++ src/test/java/com/ryewoncode/mybatisRestApi2/MybatisRestApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryewoncode.mybatisRestApi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisRestApi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
